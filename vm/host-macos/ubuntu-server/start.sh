1886846c6e57c54152eee372c4e3c08584fc0104
Saverio Ferrara <fsferrara@users.noreply.github.com>
2025-12-23 21:51:09 +0100

    feat: ubuntu-server config draft

---
+++ vm/host-macos/ubuntu-server/start.sh
@@ -0,0 +1,60 @@
+#! /usr/bin/env bash
+
+set -e
+set -u
+set -o pipefail
+
+# QEMU Arguments
+qemu_args=(
+  # Uses the macOS Hypervisor
+  -accel hvf
+  # RAM size
+  -m 16G
+  # CPU cores
+  -smp 4
+  # Uses the host CPU model
+  -cpu host
+  # Machine type
+  -M virt
+  # UEFI firmware for ARM64
+  -bios /opt/homebrew/share/qemu/edk2-aarch64-code.fd
+  # Disk image
+  -drive if=virtio,format=qcow2,file=ubuntu-server.qcow2
+  # Slot 1 - network card
+  -device virtio-net-pci,netdev=net0,addr=0x1
+  # Slot 2 - Virtio GPU
+  -device virtio-gpu-pci,addr=0x2
+  # Slot 3 - USB card
+  -device qemu-xhci,addr=0x3
+  # Display settings
+  -display default
+  # USB keyboard and mouse
+  -device usb-kbd
+  # USB tablet for better mouse handling
+  -device usb-tablet
+  # Network with SSH port forwarding (Host 2222 -> Guest 22)
+  -netdev user,id=net0,hostfwd=tcp::2222-:22
+  # Shared Folder
+  -virtfs local,path=../../share,mount_tag=share,security_model=mapped-file,id=share,readonly=off
+  -virtfs local,path=../../../shell,mount_tag=shell,security_model=mapped-file,id=shell,readonly=on
+  -virtfs local,path=../../../software,mount_tag=software,security_model=mapped-file,id=software,readonly=on
+  # Installation ISO
+  -cdrom ubuntu-24.04.3-live-server-arm64.iso
+)
+
+echo ""
+echo "Starting QEMU VM with the following shared folders: share, shell, software"
+echo ""
+echo "To mount shared folder in the guest VM, use:"
+echo "sudo mkdir -p /mnt/share"
+echo "sudo mount -v -t 9p -o trans=virtio,version=9p2000.L,dfltuid=\$(id -u),dfltgid=\$(id -g) share /mnt/share"
+echo "sudo chown \$(id -u):\$(id -g) /mnt/share"
+echo ""
+echo "To make it permanent, add this line to /etc/fstab:"
+echo "share  /mnt/share  9p trans=virtio,version=9p2000.L,rw,_netdev,nofail,users  0  0"
+echo ""
+
+# Start the VM
+qemu-system-aarch64 "${qemu_args[@]}"
+
+
