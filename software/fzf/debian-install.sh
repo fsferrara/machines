56f50f729152b8f73712d0de1c544064c6cf338b
Saverio Ferrara <fsferrara@users.noreply.github.com>
2025-12-28 15:56:05 +0100

    feat: fzf config

---
+++ software/fzf/debian-install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+
+##########
+# CONFIG #
+##########
+CMD_UPDATE="sudo apt update"
+CMD_INSTALL="sudo apt install -y"
+
+
+##########
+# UPDATE #
+##########
+echo -e "\n\n🛸 Updading...\n";
+${CMD_UPDATE}
+
+
+###########
+# INSTALL #
+###########
+echo -e "\n\n🚀 Installing...\n";
+${CMD_INSTALL} fzf
+
+
+########
+# DONE #
+########
+echo -e "\n\n✅ Done!\n";
+exit 0;
