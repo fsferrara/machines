8045ac39279503d672c69008ba5e652c14ed5827
Saverio Ferrara <fsferrara@users.noreply.github.com>
2026-01-29 15:23:35 +0100

    feat: make ZSH more like BASH

---
+++ shell/config/.my-shell-conf/confs/zsh/adapt.sh
@@ -0,0 +1,28 @@
+################################################################################
+# Config ZSH to behave like Bash in certain aspects
+################################################################################
+
+# Comments
+setopt interactivecomments
+# Correct minor spelling errors in path names when using `cd`
+setopt correct
+# Allow changing to a directory by just typing its name if it's in the CDPATH
+setopt auto_cd
+# Share command history across all sessions
+setopt share_history
+# Extended globbing features: ** for recursive matching, etc.
+setopt extended_glob
+# Treat unset variables as an error when substituting
+setopt nounset
+# Enable recursive globbing with **
+setopt globstar
+# Make glob patterns match dotfiles (hidden files starting with .)
+setopt glob_dots
+# Disable the beep sound on errors
+setopt no_beep
+# Don't error when glob patterns don't match any files (pass them literally like Bash)
+setopt no_nomatch
+# Perform word splitting on unquoted parameter expansions (like Bash/sh)
+setopt sh_word_split
+# Make regex operator =~ set BASH_REMATCH array (like Bash)
+setopt bash_rematch
--- shell/config/.my-shell-conf/confs/zsh/speed-up-zsh.sh
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Speed up Oh-my-zsh shell
-################################################################################
-
-# Uncomment the following line if you want to disable marking untracked files
-# under VCS as dirty. This makes repository status check for large repositories
-# much, much faster.
-DISABLE_UNTRACKED_FILES_DIRTY="true"
