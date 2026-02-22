fa951e9b0f4fa3bdba352f0fcebb41d8cb151292
Saverio Ferrara <fsferrara@users.noreply.github.com>
2026-02-22 15:07:51 +0100

    fix: check known_hosts file before using it

--- shell/config/.my-shell-conf/confs/bash/ssh-autocomplete.sh
+++ shell/config/.my-shell-conf/confs/bash/ssh-autocomplete.sh
@@ -2,4 +2,7 @@
 # SSH AUTOCOMPLETE.
 ################################################################################
 
-complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["`;)" ssh
+# fix: check ~/.ssh/known_hosts exists before running the command
+if [ -f ~/.ssh/known_hosts ]; then
+    complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["`;)" ssh
+fi
