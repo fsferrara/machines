70acef009b2920d55c4d2465ad193a93a1259f77
Saverio Ferrara <fsferrara@users.noreply.github.com>
2026-04-18 15:07:40 +0200

    feat: add ollama feature with installation and configuration scripts for macOS and Ubuntu

---
+++ features/ollama/install/macosx.sh
@@ -0,0 +1,32 @@
+#! /usr/bin/env sh
+
+set -e
+set -u
+
+
+##########
+# CONFIG #
+##########
+CMD_UPDATE="brew update"
+CMD_INSTALL="brew install"
+
+
+##########
+# UPDATE #
+##########
+printf '\n\n🛸 Updading...\n'
+${CMD_UPDATE}
+
+
+###########
+# INSTALL #
+###########
+printf '\n\n🚀 Installing...\n'
+${CMD_INSTALL} ollama
+
+
+########
+# DONE #
+########
+printf '\n\n✅ Done!\n'
+exit 0;
---
+++ features/ollama/install/ubuntu.sh
@@ -0,0 +1,32 @@
+#! /usr/bin/env sh
+
+set -e
+set -u
+
+
+##########
+# CONFIG #
+##########
+CMD_UPDATE="sudo apt update"
+CMD_INSTALL="sudo apt install -y"
+
+
+##########
+# UPDATE #
+##########
+printf '\n\n🛸 Updading...\n'
+${CMD_UPDATE}
+
+
+###########
+# INSTALL #
+###########
+printf '\n\n🚀 Installing...\n'
+${CMD_INSTALL} ollama
+
+
+########
+# DONE #
+########
+printf '\n\n✅ Done!\n'
+exit 0;
