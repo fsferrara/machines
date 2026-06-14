7950beb0fd06056f62f92b2ad3270a903ca67949
Saverio Ferrara <fsferrara@users.noreply.github.com>
2026-06-14 09:40:43 +0200

    feat: add ghostty configuration

---
+++ features/ghostty/install/macosx.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/env sh
+
+
+##########
+# CONFIG #
+##########
+CMD_UPDATE="brew update"
+CMD_INSTALL="brew install"
+
+
+##########
+# UPDATE #
+##########
+printf '\n\n🛸 Updading...\n'
+${CMD_UPDATE}
+
+
+###########
+# INSTALL #
+###########
+printf '\n\n🚀 Installing...\n'
+
+${CMD_INSTALL} --cask ghostty
+
+
+########
+# DONE #
+########
+printf '\n\n✅ Done!\n'
+exit 0;
