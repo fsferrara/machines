30a74cef962029cd86429b181049c89bafb393c2
Saverio Ferrara <fsferrara@users.noreply.github.com>
2026-07-03 08:35:05 +0200

    feat: cleanup utility for mac

--- documentation/macosx/install_casks_for_tools_and_tech.sh
+++ documentation/macosx/install_casks_for_tools_and_tech.sh
@@ -3,7 +3,6 @@
 set -e
 set -u
 
-
 install_casks_for_tools_and_tech() {
 
   # [CASK] Development Essentials
@@ -79,7 +78,8 @@ install_casks_for_tools_and_tech() {
   #brew install --cask sdformatter
   #brew install --cask balenaetcher
   #brew install --cask raspberry-pi-imager
-  brew install mac-cleanup-py
+  brew install mac-cleanup-py # cleanup utility
+  brew install mole           # cleanup utility
   brew install --cask android-file-transfer
   brew install --cask the-unarchiver
 
