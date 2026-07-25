10d1db19c1c87d8b71113d55e21087b55620dd75
Saverio Ferrara <fsferrara@users.noreply.github.com>
2026-07-25 09:41:02 +0200

    feat: add alias for detached tmux session creation

--- features/tmux/config/.my-shell-conf/confs/generic/tmux.sh
+++ features/tmux/config/.my-shell-conf/confs/generic/tmux.sh
@@ -6,6 +6,7 @@
 alias tmux-list="tmux ls"
 alias tmux-attach="tmux attach -t"
 alias tmux-new="tmux new -s"
+alias tmux-new-detached="tmux new -d -s"
 alias tmux-kill="tmux kill-session -t"
 alias tmux-kill-all="tmux kill-server"
 
