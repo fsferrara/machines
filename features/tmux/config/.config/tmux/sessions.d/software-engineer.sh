9f1837e6abe7b3b6cee188c63519dd931e504ad3
Saverio Ferrara <fsferrara@users.noreply.github.com>
2026-09-22 12:31:38 +0200

    feat: tmux sessions definition for engineers

---
+++ features/tmux/config/.config/tmux/sessions.d/software-engineer.sh
@@ -0,0 +1,15 @@
+TMUX_SESSION_DIRECTORY="$MAEW_AGENTS/software-engineer"
+
+tmux_session_create() {
+  session_name=$1
+  session_directory=$2
+  agent_instance_command='codex'
+
+  tmux new-session -d -s "$session_name" -n agent_home -c "$session_directory"
+  tmux send-keys -t "$session_name:agent_home" "nvim $session_directory" C-m
+
+  tmux new-window -t "$session_name" -n default_agent_instance -c "$session_directory"
+  tmux send-keys -t "$session_name:default_agent_instance" "$agent_instance_command" C-m
+
+  tmux select-window -t "$session_name:default_agent_instance"
+}
